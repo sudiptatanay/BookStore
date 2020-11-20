9b944923d697fc762849c5acb2d28cca2e4488bd
Sudipta Das <sudipta2812@live.in>
2020-11-20 13:51:52 +0530

    libman added

---
+++ WebgentleBookStore/WebgentleBookStore/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebgentleBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View("TempViews/ContactUs.cshtml");
+        }
+
+    }
+}
--- WebgentleBookStore/WebgentleBookStore/Startup.cs
+++ WebgentleBookStore/WebgentleBookStore/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace WebgentleBookStore
@@ -16,6 +18,8 @@ namespace WebgentleBookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +31,20 @@ namespace WebgentleBookStore
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
 
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory() , "MyStaticFiles")),
+                RequestPath= "/MyStaticFiles"
+
+            }) ;
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+          
         }
     }
 }
